0439f9a2ddf886d89d651da12e4a5f2a76c74e87
MirzalievPulat <mirzaliyevpolat3105@gmail.com>
2022-12-11 15:03:26 +0500

    returns "Thank You"

--- app/src/main/java/com/example/passobjectkotlin/MainActivity.kt
+++ app/src/main/java/com/example/passobjectkotlin/MainActivity.kt
@@ -4,24 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.passobjectkotlin.model.User
 
 class MainActivity : AppCompatActivity() {
+    lateinit var text:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initviews()
     }
     fun initviews(){
+        text = findViewById<TextView>(R.id.text)
         var button = findViewById<Button>(R.id.button)
         button.setOnClickListener{
             var user = User("Po'lat", "20")
             openSecondPage(user)
         }
     }
+
+    var launchSecond = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) { result ->
+        if(result.resultCode == RESULT_OK){
+            val data:Intent? = result.data
+            var text2 = data!!.getStringExtra("result")
+            text.text = text2
+        }
+    }
+
     fun openSecondPage(user:User){
         var intent = Intent(this,SecondActivity::class.java)
         intent.putExtra("user",user)
-        startActivity(intent)
+        launchSecond.launch(intent)
     }
 }
--- app/src/main/java/com/example/passobjectkotlin/SecondActivity.kt
+++ app/src/main/java/com/example/passobjectkotlin/SecondActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import com.example.passobjectkotlin.model.User
 import org.w3c.dom.Text
@@ -18,10 +20,21 @@ class SecondActivity : AppCompatActivity() {
     }
     fun initviews(){
         val text = findViewById<TextView>(R.id.text)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            openBack()
+        }
 
         val user = intent.getParcelableExtra<User>("user")
         Log.d(TAG,user.toString())
 
-        text.setText(user.toString())
+        text.text = user.toString()
+    }
+
+    fun openBack(){
+        val intent = Intent()
+        intent.putExtra("result", "Thank You")
+        setResult(RESULT_OK,intent)
+        finish()
     }
 }
